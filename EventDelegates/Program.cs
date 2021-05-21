4124d993fe138ef915bd5847bb936dc923d0c7b5
eslam ahmed ali hassan <eslam.pop911@yahoo.com>
2021-05-21 15:25:38 +0200

    Event + Delegates + custom EventArgs class

--- EventDelegates/Program.cs
+++ EventDelegates/Program.cs
@@ -5,9 +5,6 @@ namespace EventDelegates
     class Program
     {
                 
-        public delegate void WorkPerformedHandler (int hours);
-
-        public event WorkPerformedHandler WorkEvent;
 
         static void Main(string[] args)
         {
@@ -15,32 +12,45 @@ namespace EventDelegates
             WorkPerformedHandler handler2 = WorkPerformed2;
             WorkPerformedHandler handler3 = WorkPerformed3;
 
-        
+            // here we can subscribe to an event
+            Worker worker = new Worker();
+            worker.WorkCompleted += Workcompleted;
+            worker.WorkPerformed += WorkPerformed1;
+
+
+            worker.DoWork(5);
+
+
+            //// there are here 3 seperate invocatoin list in 3 seperate multicast delegates
+            //handler += handler2 + handler3; // handler 2 will not invoke the invocatoin list from the below code.
+
+            //handler2 += handler3+handler2;
+            //handler2 += handler3;
             
-            // there are here 3 seperate invocatoin list in 3 seperate multicast delegates
-            handler += handler2 + handler3; // handler 2 will not invoke the invocatoin list from the below code.
+            ////handler += handler2 + handler3; handler 2 will invoke all hanler 2 from the above
 
-            handler2 += handler3+handler2;
-            handler2 += handler3;
-            
-            //handler += handler2 + handler3; handler 2 will invoke all hanler 2 from the above
+            //handler3 += handler;
 
-            handler3 += handler;
-
-            Console.WriteLine("handler 1 invocatrion list\n");
-            DoWork(handler); //invoked the methode 2 times
-            Console.WriteLine("\n");
-            Console.WriteLine("handler 2 invocatrion list\n");
-            DoWork(handler2);// invokes it only one time 
-            Console.WriteLine("\n");
-            Console.WriteLine("handler 3 invocatrion list\n");
-            DoWork(handler3);// invokes it only one time 
-            //handler = WorkPerformed1;;
-            handler += handler2 + handler3;
-            Console.WriteLine("\n");
-            Console.WriteLine("AGAIN: handler 1 invocatrion list\n");
-            DoWork(handler); //invoked the methode 2 times
+            //Console.WriteLine("handler 1 invocatrion list\n");
+            //DoWork(handler); //invoked the methode 2 times
+            //Console.WriteLine("\n");
+            //Console.WriteLine("handler 2 invocatrion list\n");
+            //DoWork(handler2);// invokes it only one time 
+            //Console.WriteLine("\n");
+            //Console.WriteLine("handler 3 invocatrion list\n");
+            //DoWork(handler3);// invokes it only one time 
+            ////handler = WorkPerformed1;;
+            //handler += handler2 + handler3;
+            //Console.WriteLine("\n");
+            //Console.WriteLine("AGAIN: handler 1 invocatrion list\n");
+            //DoWork(handler); //invoked the methode 2 times
         }
+
+        private static void Workcompleted(object? sender, WorkPerformedEventArgs e)
+        {
+            Console.WriteLine($"workcompleted and the number of hours is {e.Hours}");
+        }
+
         // this is a dynamic method
         static void DoWork(WorkPerformedHandler handler)
         { 
---
+++ EventDelegates/Worker.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventDelegates
+{
+    public delegate void WorkPerformedHandler (int hours);
+
+    class Worker
+    {
+        public event WorkPerformedHandler WorkPerformed;
+        //user custom event args with a class that extent the EventArgs class with EventHandler<T>
+        public event EventHandler<WorkPerformedEventArgs> WorkCompleted;
+        public void DoWork(int hours)
+        {
+            for (int i = 0; i < hours; i++)
+            {
+                //raise event 
+                OnWorkPerformed(i + 1);
+            }
+            //raise event
+            OnWorkCompleted( hours);
+        }
+
+        protected virtual void OnWorkPerformed(int hours)
+        {
+            WorkPerformed?.Invoke(hours);
+        }
+
+        protected virtual void OnWorkCompleted(int  hours)
+        {
+            WorkPerformedEventArgs wpea = new WorkPerformedEventArgs {Hours = hours};
+            WorkCompleted?.Invoke(this, wpea);
+        }
+    }
+}
